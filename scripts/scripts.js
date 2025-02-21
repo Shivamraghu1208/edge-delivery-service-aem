3d7c8ebb384b9668dde22b18083c005ba1048f92
Shivam Raghuwanshi <shivam.raghuwanshi@aurx.co>
2025-02-21 16:58:14 +0530

    update file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -133,7 +133,7 @@ setTimeout(() => {
     console.log('Href link disabled, custom logic will run now.');
  
  });
- }, 5000);
+ }, 500);
 
  document.addEventListener("DOMContentLoaded", (event)=>{
   console.log("Dom content loaded");
